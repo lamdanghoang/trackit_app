cf981ada6f66fa740d92e2dd7dd1c089f7c8403f
Dang Hoang Lan <hoanglam@Dangs-MacBook-Pro.local>
2025-03-27 00:47:08 +0700

    update css

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -12,17 +12,17 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      "relative h-4 w-full overflow-hidden rounded-full bg-transparent",
       className
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="absolute h-full w-full flex-1 bg-green-500 transition-all left-0"
+      className="absolute h-full w-full flex-1 bg-green-500/60 transition-all left-0"
       style={{ transform: `translateX(${100 - (value || 0)}%)` }}
     />
     <ProgressPrimitive.Indicator
-      className="absolute h-full w-full flex-1 bg-red-500 transition-all right-0"
+      className="absolute h-full w-full flex-1 bg-red-500/60 transition-all right-0"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
