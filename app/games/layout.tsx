ca135092a27860631d58394e61dd05bf72b5e33e
Dang Hoang Lan <hoanglam@Dangs-MacBook-Pro.local>
2025-04-02 16:48:20 +0700

    add games page, update some css

--- app/games/layout.tsx
+++ app/games/layout.tsx
@@ -1,13 +1,18 @@
-import Tracker from "@/components/page/Tracker/index";
+"use client";
+
 import GlobalContext from "@/context/store";
 import { useContext, useEffect } from "react";
 
-export default function TrackerPage() {
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   const { setSelectedNav } = useContext(GlobalContext);
 
   useEffect(() => {
-    setSelectedNav("Tracker");
+    setSelectedNav("Games");
   }, []);
 
-  return <Tracker />;
+  return <>{children}</>;
 }
